de0871826662ef4d3d18369a28dcdd14cbb69353
于洋 <1317030038@qq.com>
2021-09-16 13:26:07 +0800

    安装xiaobug

--- vue.config.js
+++ vue.config.js
@@ -26,19 +26,19 @@ module.exports = {
                     "from": "./extraResources/",
                     "to": "extraResources"
                 },
-                // "nsis": {
-                //     "oneClick": false, // 一键安装
-                //     // "guid": "xxxx", //注册表名字，不推荐修改
-                //     "perMachine": true, // 是否开启安装时权限限制（此电脑或当前用户）
-                //     "allowElevation": true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
-                //     "allowToChangeInstallationDirectory": true, // 允许修改安装目录
-                //     "installerIcon": "./dist_electron/icons/icon.ico", // 安装图标
-                //     "uninstallerIcon": "./dist_electron/icons/icon.ico", //卸载图标
-                //     "installerHeaderIcon": "./dist_electron/icons/icon.ico", // 安装时头部图标
-                //     "createDesktopShortcut": true, // 创建桌面图标
-                //     "createStartMenuShortcut": false, // 创建开始菜单图标
-                //     "shortcutName": "Latex题库0.0.5" // 图标名称
-                // }
+                "nsis": {
+                    "oneClick": false, // 一键安装
+                    // "guid": "xxxx", //注册表名字，不推荐修改
+                    "perMachine": true, // 是否开启安装时权限限制（此电脑或当前用户）
+                    "allowElevation": true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+                    "allowToChangeInstallationDirectory": true, // 允许修改安装目录
+                    "installerIcon": "./dist_electron/icons/icon.ico", // 安装图标
+                    "uninstallerIcon": "./dist_electron/icons/icon.ico", //卸载图标
+                    "installerHeaderIcon": "./dist_electron/icons/icon.ico", // 安装时头部图标
+                    "createDesktopShortcut": true, // 创建桌面图标
+                    "createStartMenuShortcut": false, // 创建开始菜单图标
+                    "shortcutName": "Latex题库0.0.5" // 图标名称
+                }
 
             }
         }
